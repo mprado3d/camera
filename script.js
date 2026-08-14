447f38c7c7e752467ebc20b7fb04d8ad303f18a3
mprado3d <mprado@netflix.com>
2026-08-14 10:30:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const camera = document.getElementById("camera");
 const startButton = document.getElementById("startCamera");
 const captureButton = document.getElementById("captureButton");
 
+let cameraStream = null;
+
 startButton.addEventListener("click", async () => {
 
   try {
 
-    const stream = await navigator.mediaDevices.getUserMedia({
+    cameraStream = await navigator.mediaDevices.getUserMedia({
       video: {
         facingMode: "user"
       },
       audio: false
     });
 
-    camera.srcObject = stream;
+    camera.srcObject = cameraStream;
 
     startButton.style.display = "none";
     captureButton.style.display = "block";
@@ -21,8 +23,51 @@ startButton.addEventListener("click", async () => {
   } catch (error) {
 
     alert("Could not access camera.");
-
     console.error(error);
+
   }
+
+});
+
+
+captureButton.addEventListener("click", () => {
+
+  // Camera flash
+  const flash = document.createElement("div");
+
+  flash.style.position = "fixed";
+  flash.style.inset = "0";
+  flash.style.background = "white";
+  flash.style.zIndex = "9999";
+  flash.style.opacity = "1";
+  flash.style.transition = "opacity 250ms ease";
+
+  document.body.appendChild(flash);
+
+  // Hide shutter button
+  captureButton.style.display = "none";
+
+  // Stop camera
+  if (cameraStream) {
+
+    cameraStream.getTracks().forEach(track => {
+      track.stop();
+    });
+
+  }
+
+  // Fade flash
+  setTimeout(() => {
+
+    flash.style.opacity = "0";
+
+  }, 100);
+
+  // Remove flash
+  setTimeout(() => {
+
+    flash.remove();
+
+  }, 400);
 
 });
